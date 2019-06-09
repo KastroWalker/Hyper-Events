b22c77ed591f741f34a17248384df8d753b6ec43
kauanphbbb <kauanphbbb@gmail.com>
2019-06-08 21:33:18 -0300

    Banco feito, aguardando o grupo para visualizar se tudo está correto

--- SQL/Banco.sql
+++ SQL/Banco.sql
@@ -13,7 +13,7 @@ create table participantes(
 	email varchar(60) not null,
 	usuario varchar(200) not null,
 	senha varchar(20) not null,
-	primary key(idUser)
+	primary key(idPart)
 );
 
 -- Criando a tabela organizadores --
@@ -49,6 +49,32 @@ create table ministrantes(
 	primary key(idMin)
 );
 
+-- Criando a tabela de contatos --
+create table contatos(
+	idCont int not null auto_increment,
+	nome varchar(200) not null,
+	email varchar(200),
+	telefone varchar(10),
+	primary key(idCont)
+);
+
+-- Criando a tabela de materiais --
+create table materiais(
+	idMat int not null auto_increment,
+	nome varchar(200) not null,
+	tipo varchar(200) not null,
+	quantidade int not null,
+	primary key(idMat)
+);
+
+-- Criando a tabela de sócios --
+create table socios(
+	idSoc int not null auto_increment,
+	nome varchar(200) not null,
+	informacoes varchar(500) not null, -- Eu fiquei sem ideia aqui kk
+	primary key(idSoc)
+);
+
 -- Criando a tabela de minicursos --
 create table minicursos(
 	idMinicurso int not null auto_increment,
@@ -81,10 +107,31 @@ create table locais(
 	nome varchar(200) not null,
 	endereco varchar(200) not null,
 	cep varchar(8) not null,
+	descricao varchar(500), -- Descrição do local
+	primary key(idLoc)
 );
 -- Criando a tabela de eventos --
 create table eventos(
 	idEvent int not null auto_increment,
-	titulo varchar(200) int not null,
-
+	idPart int not null,
+	idOrg int not null,
+	idPal int not null,
+	idMin int not null,
+	idPalestras int not null,
+	idMinicurso int not null,
+	idLoc int not null,
+	idCont int not null,
+	idSoc int not null,
+	idMat int not null,
+	titulo varchar(200) not null,
+	primary key(idEvent),
+	foreign key(idPart) references participantes(idPart),
+	foreign key(idOrg) references organizadores(idOrg),
+	foreign key(idPal) references palestrantes(idPal),
+	foreign key(idMin) references ministrantes(idMin),
+	foreign key(idPalestras) references palestras(idPalestras),
+	foreign key(idLoc) references locais(idLoc),
+	foreign key(idCont) references contatos(idCont),
+	foreign key(idSoc) references socios(idSoc),
+	foreign key(idMat) references materiais(idMat)
 );
