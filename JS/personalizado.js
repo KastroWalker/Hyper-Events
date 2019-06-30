622def791be55c692f563665cb599b1cf2b0f732
KastroWalker <victorcsa2002@gmail.com>
2019-06-30 00:34:58 -0300

    Adicionando o Jquery

---
+++ JS/personalizado.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('a[data-confirm]').click(function(ev){
+		var href = $(this).attr('href');
+		if(!$('#confirm-delete').length){
+			$('body').append('<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">EXCLUIR ITEM<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><p class="alert alert-danger">Deseja mesmo Excluir o Evento?</p></div><div class="modal-footer"><button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button><a class="btn btn-danger text-white" id="dataComfirmOK">Excluir</a></div></div></div></div>');
+		}
+		$('#dataComfirmOK').attr('href', href);
+        $('#confirm-delete').modal({show: true});
+		return false;
+	});
+});
