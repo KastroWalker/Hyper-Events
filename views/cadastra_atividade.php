0e2bb185ebabb6c5f57af3b62f8408366aeca761
KastroWalker <victorcsa2002@gmail.com>
2019-07-23 22:48:32 -0300

    Criando a opção de Cadastrar palestrante

--- views/cadastra_atividade.php
+++ views/cadastra_atividade.php
@@ -12,7 +12,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="Victor Castro">
         <link rel="stylesheet" type="text/css" href="../CSS/bootstrap/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="../CSS/style_cadastro_eveto.css">
         <style type="text/css">
             header, footer, #Manual {
                 text-align: center;
---
+++ views/cadastro_ministrante.php
@@ -0,0 +1,45 @@
+<?php 
+	session_start();
+
+	include_once '../Controls/verifica_login.php';
+	$tipo_mini = $_REQUEST['tipo_mini'];
+	if ($tipo_mini == 'palestrante') {
+		$pag = 'palestra';
+	} else if ($tipo_mini == 'ministrante') {
+		$pag = 'minicurso';
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Victor Castro">
+        <link rel="stylesheet" type="text/css" href="../CSS/bootstrap/bootstrap.min.css">
+        <style type="text/css">
+            header, footer, #Manual {
+                text-align: center;
+            }
+        </style>
+        <script src="../JS/valida_cadastro_evento.js"></script>
+        <title>Cadastrar Evento - Hyper-Events</title>
+    </head>
+    <body>
+    	<div class="container">
+			<?php 
+	            require_once 'header.php';
+	        ?>
+	    	<form>
+
+		        <input type="submit" name="btn_enviar" id="btn_enviar" value="Cadastrar" class="btn btn-primary">
+		        <input type="reset" name="btn_limpar" id="btn_limpar" value="Limpar" class="btn btn-secondary">
+		        <a href="cadastra_atividade.php?tipo=<?php echo $pag; ?>" id="btn_voltar" name="btn_voltar" class="btn btn-info">Voltar</a>
+	        </form>
+	        <?php 
+	        	# Importa o rodape padrão
+	            require_once 'footer.php';
+	        ?>
+		</div>
+    </body>
+</html>
