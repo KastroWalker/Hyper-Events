5006fade0940b4e97437c6e28ce0021f54f91f07
KastroWalker <victorcsa2002@gmail.com>
2019-08-29 12:44:34 -0300

    Finalinzanco cadastro de usuario no evento

--- Controls/cadastrar_em_evento.php
+++ Controls/cadastrar_em_evento.php
@@ -1,8 +1,23 @@
 <?php
+	include 'conexao.php';
 	$id = $_REQUEST['id'];
 	$user_id = $_REQUEST['user_id'];
 	echo "$id";
 	echo "$user_id";
 
-	$sql = "insert into eventos_participantes(evento_id, part_id) values('{$id},' '{$user_id}');";
+	$sql = "insert into eventos_participantes(part_id, evento_id) values('{$user_id}', '{$id}');";
+
+	if($conexao->query($sql) === TRUE){
+		$_SESSION['cadastrado_no_evento'];
+		header('Location: ../views/area_user.php');
+		exit();
+		echo "Cadastrado com sucesso";
+	}else{
+		$_SESSION['nao_cadastrado_no_evento'];
+		header('Location: ../views/area_user.php');
+		exit();
+		echo "Erro ao cadastrar ".mysqli_error($conexao);
+	}
+
+	$conexao->close();
 ?>
