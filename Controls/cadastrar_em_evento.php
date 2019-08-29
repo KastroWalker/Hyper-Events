66098c1daaf4f873f5a66feb8ae6ce7a7c6d3fbc
KastroWalker <victorcsa2002@gmail.com>
2019-08-29 12:06:10 -0300

    Upandado cadastro de usuario no evento

--- Controls/cadastrar_em_evento.php
+++ Controls/cadastrar_em_evento.php
@@ -1,6 +1,8 @@
 <?php
-	$id = $_RESQUEST['id'];
-	$user_id = $_RESQUEST['user_id'];
+	$id = $_REQUEST['id'];
+	$user_id = $_REQUEST['user_id'];
 	echo "$id";
 	echo "$user_id";
+
+	$sql = "insert into eventos_participantes(evento_id, part_id) values('{$id},' '{$user_id}');";
 ?>
