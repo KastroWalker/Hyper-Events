d7077aa76c722b337f881edab514994ac1a16146
KastroWalker <victorcsa2002@gmail.com>
2019-09-13 16:01:02 -0300

    Modificando configurações de conexao

--- Controls/conexao.php
+++ Controls/conexao.php
@@ -3,10 +3,10 @@
     Conectando com o Banco de Dados
     Criador por: Victor Castro  
     */
-    define('HOST', 'localhost:3306');
-    define('USER', 'matue');
-    define('SENHA', 'banco');
-    define('BD', 'HyperEvents');
+    $HOST = 'localhost';
+    $USER = 'root';
+    $SENHA = '';
+    $BD = 'HyperEvents';
 
-    $conexao = mysqli_connect(HOST, USER, SENHA, BD) or die ('Não foi possivel conectar');
+    $conexao = mysqli_connect($HOST, $USER, $SENHA, $BD) or die ('Não foi possivel conectar');
 ?>
