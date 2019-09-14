23a9f84d4743c8280977c3657614fa9e2e15afc5
Kauan <kauanphbbb@gmail.com>
2019-09-13 21:06:34 -0300

    Remodelando o banco de dados

--- SQL/Banco-Kauan.sql
+++ SQL/Banco-Kauan.sql
@@ -6,40 +6,37 @@
 -- Criando a base de dados --
 create database HyperEvents2;
 
-use HyperEvents2;
+    use HyperEvents2;
 
 -- Dando o privilegio para o usuário padrão --
 grant all privileges on HyperEvents2.* to 'matue'@'%';
 
+-- Criando tabela de tipos de usuário --
+create table tipoUsuario(
+    idtipo_usuario int not null auto_increment,
+    nome varchar(60) not null,
+    primary key(idtipo_usuario)
+);
+
+-- Inserindo usuários padrões --
+insert into tipoUsuario (nome) values ("Organizador"); 
+insert into tipoUsuario (nome) values ("Participante"); 
+insert into tipoUsuario (nome) values ("Palestrante");
+
 -- Criando tabela de usuário --
 create table usuario(
     user_id int not null auto_increment,
+    idtipo_usuario int not null,
     nome varchar(200) not null,
-    data_nasc date not null,
-    idade int not null,
-    sexo char(1) not null,
-    cpf varchar(11) not null,
+    sexo char(1),
+    cpf varchar(11),
     usuario varchar(100) not null,
     senha varchar(32) not null,
     email varchar(100) not null,
     contato varchar(12),
-    primary key(user_id)
+    primary key(user_id),
+    foreign key (idtipo_usuario) references tipoUsuario (idtipo_usuario)
 );
-
--- Criando tabela de palestrantes --
-create table palestrante(
-    palestrante_id int not null auto_increment,
-    nome varchar(200) not null,
-    data_nasc date not null,
-    idade int not null,
-    sexo char(1) not null,
-    cpf varchar(11) not null,
-    usuario varchar(100) not null,
-    senha varchar(32) not null,
-    email varchar(100) not null,
-    contato varchar(12),
-    primary key(user_id)
-)
 
 -- Criando tabela de eventos --
 create table eventos(
@@ -48,7 +45,6 @@ create table eventos(
     titulo varchar(200) not null,
     descricao varchar(500) not null,
     url_evento varchar(200),
-    hora_inicio time not null,
     data_inicio date not null,
     data_fim date not null,
     email varchar(100) not null,
@@ -56,40 +52,36 @@ create table eventos(
     foreign key(user_id) references usuario(user_id)
 );
 
--- Criando a tabela de palestra --
-create table palestra(
-    palestra_id int not null auto_increment,
+-- Criando tabela de tipos de atividade --
+create table tipoAtividade (
+    idTipoAtividade int not null auto_increment,
+    titulo varchar(100) not null,
+    primary key(idTipoAtividade)
+);
+
+-- Criando tabela para convidado --
+create table convidado(
+    idConvidado int not null auto_increment,
+    nome varchar(100) not null,
+    descricao varchar(250),
+    email varchar(60),
+    primary key(idConvidado)
+);
+
+-- Criando a tabela de atividades --
+create table atividade(
+    atividade_id int not null auto_increment,
     evento_id int not null,
+    idTipoAtividade int not null,
+    idConvidado int,
     nome varchar(20) not null,
-    descricao varchar(200) not null, 
-    data_inicio date not null,
-    data_fim date not null,
-    local varchar(30) not null,
+    descricao varchar(500) not null, 
+    data date not null,
+    local varchar(60),
     inicio time not null, 
     fim time not null,
-    primary key(palestra_id),
-    foreign key(evento_id) references eventos(evento_id)
+    primary key(atividade_id),
+    foreign key(evento_id) references eventos(evento_id),
+    foreign key(idTipoAtividade) references tipoAtividade(idTipoAtividade),
+    foreign key(idConvidado) references convidado(idConvidado)
 );
-
--- Adicionado a chave estrangeira de palestra na tabela do evento --
-alter table eventos add palestra_id int after data_fim;
-alter table eventos add foreign key(palestra_id) references palestra(palestra_id);
-
--- Criando a tabela de minicurso --
-create table minicursos(
-    minicurso_id int not null auto_increment,
-    evento_id int not null,
-    nome varchar(200) not null,
-    descricao varchar(500) not null, 
-    data_inicio date not null,
-    data_fim date not null,
-    local varchar(30) not null,
-    inicio time not null, 
-    fim time not null, 
-    primary key(minicurso_id),
-    foreign key(evento_id) references eventos(evento_id)
-);
-
--- Adicionado a chave estrangeira de minicurso na tabela do evento --
-alter table eventos add minicurso_id int after palestra_id;
-alter table eventos add foreign key(minicurso_id) references minicursos(minicurso_id);
