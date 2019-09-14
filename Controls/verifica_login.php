341d78dc0cd76093868fc5a7bb28bc230756ba9c
Kauan <kauanphbbb@gmail.com>
2019-09-14 14:36:19 -0300

    Redirecionando o login para a página de login ao invés da página inicial

--- Controls/verifica_login.php
+++ Controls/verifica_login.php
@@ -4,7 +4,7 @@
     Criado por: Victor Castro
     */
     if (!$_SESSION['usuario']) {
-        header('Location: ../index.php');
+        header('Location: ../views/login.php');
         exit();
     }
 ?>
