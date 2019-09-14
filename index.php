3d903f093052a9acdad010df6ae4b098fc1aff80
Kauan <kauanphbbb@gmail.com>
2019-09-14 10:49:17 -0300

    Alterando tela inicial

--- index.php
+++ index.php
@@ -5,17 +5,32 @@
 	<meta name="robots" content="index, follow"/>
 	<meta name="description" content="Hyper Events - Sistema de Eventos Acadêmicos"/>
 	<meta name="keywords" content="Eventos Acadêmicos, Escola,"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link rel="icon" href="img/icon.png" type="image/x-icon"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<link rel="icon" href="img/icon.png" type="image/x-icon"/>
+	<link rel="stylesheet" href="CSS/style_padrao.css">
 	<title>Hyper Events</title>
 </head>
 <body>
-	<div id="menu">
-		<ul>
-			<li><a href="views/login.php">Login</a></li>
-			<li><a href="">Eventos</a></li>
-			<li><a href="">Área do organizador</a></li>
-		</ul>
-	</div>
+	<div class="div_principal">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="area_org.php">Hyper Events</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="area_org.php">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="lista_eventos.php">Eventos</a>
+					</li>  
+				</ul>
+			</div>
+			<form class="form-inline my-2 my-lg-0">
+				<a href="../Controls/logout.php" class="btn btn-danger">Sair</a>             
+			</form>
+		</nav>
 </body>
 </html>
