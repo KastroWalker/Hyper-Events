2e14da5c459e4139f5f3d6f5256b6c3dd711c37d
KastroWalker <victorcsa2002@gmail.com>
2019-09-20 17:02:09 -0300

    Adicionando coluna de hora e data de inscrição no evento

--- SQL/InnerJoin.sql
+++
@@ -1,3 +0,0 @@
-select atividade.*, tipoAtividade.titulo, convidado.nome
-from atividade inner join tipoAtividade on atividade.idTipoAtividade = tipoAtividade.idTipoAtividade
-left outer join convidado on atividade.idConvidado = convidado.idConvidado;
--- SQL/Mostrar_para_o_Denival.sql
+++ SQL/Mostrar_para_o_Denival.sql
@@ -1,5 +1,13 @@
+/*
+select atividade.*, tipoAtividade.titulo, convidado.nome
+from atividade inner join tipoAtividade on atividade.idTipoAtividade = tipoAtividade.idTipoAtividade
+left outer join convidado on atividade.idConvidado = convidado.idConvidado;
+*/
+
+/*
 select atividade.*, tipoatividade.tipo_atividade, convidado.nome_convidado
 from atividade
 inner join eventos on (atividade.evento_id = 1 and eventos.evento_id = 1)
 inner join tipoatividade on (atividade.idTipoAtividade = tipoatividade.idTipoAtividade and tipoatividade.idTipoAtividade = atividade.idTipoAtividade)
 left outer join convidado on (atividade.idConvidado = convidado.idConvidado and convidado.idConvidado = atividade.idConvidado)
+*/
