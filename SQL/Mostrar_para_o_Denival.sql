627b387ba9f99a221c489ea821394b39f618a79a
Kauan <kauanphbbb@gmail.com>
2019-09-20 17:09:22 -0300

    Ponto e vírgula esquecido

--- SQL/Mostrar_para_o_Denival.sql
+++ SQL/Mostrar_para_o_Denival.sql
@@ -2,4 +2,4 @@ select atividade.*, tipoatividade.tipo_atividade, convidado.nome_convidado
 from atividade
 inner join eventos on (atividade.evento_id = 1 and eventos.evento_id = 1)
 inner join tipoatividade on (atividade.idTipoAtividade = tipoatividade.idTipoAtividade and tipoatividade.idTipoAtividade = atividade.idTipoAtividade)
-left outer join convidado on (atividade.idConvidado = convidado.idConvidado and convidado.idConvidado = atividade.idConvidado)
+left outer join convidado on (atividade.idConvidado = convidado.idConvidado and convidado.idConvidado = atividade.idConvidado);
