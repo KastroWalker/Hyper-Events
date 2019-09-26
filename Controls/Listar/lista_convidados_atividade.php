7745f5fa51701f976aecb82283446a756596a76f
Kauan <kauanphbbb@gmail.com>
2019-09-26 12:51:24 -0300

    Pegando convidados e mostrando na tela

--- Controls/Listar/lista_convidados_atividade.php
+++ Controls/Listar/lista_convidados_atividade.php
@@ -1,6 +1,9 @@
 <?php
+	$evento_id = $_SESSION['id'];
 	include '../conexao.php';
-	$sql = "select * from convidado";
+	$sql = "select convidado.nome_convidado, convidado.idConvidado, tipoConvidado.tipo_convidado, eventos.evento_id
+	from convidado inner join tipoConvidado on (convidado.idTipoConvidado = tipoConvidado.idTipoConvidado)
+	inner join eventos on (eventos.evento_id = convidado.evento_id) and eventos.evento_id = $evento_id;";
 	$result = mysqli_query($conexao, $sql);
 	while ($tlb = mysqli_fetch_array($result)) {
 		$idConvidado = $tlb['idConvidado'];
