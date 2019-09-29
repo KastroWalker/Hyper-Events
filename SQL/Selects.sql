b7c0bf319d0f4bfff22451c45685cb40dac28f26
Kauan <kauanphbbb@gmail.com>
2019-09-29 13:38:55 -0300

    Pegando o nome do local da atividade

--- SQL/Selects.sql
+++ SQL/Selects.sql
@@ -1,7 +1,11 @@
-select atividade.*, tipoAtividade.titulo, convidado.nome
-from atividade inner join tipoAtividade on atividade.idTipoAtividade = tipoAtividade.idTipoAtividade
-left outer join convidado on atividade.idConvidado = convidado.idConvidado;
+-- Listando detalhes das atividades --
+select atividade.*, tipoAtividade.tipo_atividade, convidado.nome_convidado, local_atividade.nome_local
+from atividade inner join tipoAtividade on (atividade.idTipoAtividade = tipoAtividade.idTipoAtividade)
+inner join convidado on (convidado.idConvidado = atividade.idConvidado)
+inner join local_atividade on (local_atividade.local_id = atividade.local_id)
+inner join eventos on (eventos.evento_id = atividade.evento_id) and eventos.evento_id = 1 and atividade.atividade_id = 1;
 
+-- Listando detalhe dos convidados -- 
 select convidado.nome_convidado, convidado.idConvidado, tipoConvidado.tipo_convidado, eventos.evento_id
 from convidado inner join tipoConvidado on (convidado.idTipoConvidado = tipoConvidado.idTipoConvidado)
 inner join eventos on (eventos.evento_id = convidado.evento_id) and eventos.evento_id = 1;
