09c725cc0e05f7bc39414ed4cf0723caea310ab4
KastroWalker <victorcsa2002@gmail.com>
2019-09-28 22:38:02 -0300

    Ajeitando bug da nav bar

--- views/Eventos/Listar/lista_eventos.php
+++ views/Eventos/Listar/lista_eventos.php
@@ -31,7 +31,26 @@ if(isset($_SESSION['id'])){
         require_once '../../header_eventos.php';
         ?>
         <div class="div_principal">
-            <?php require_once '../../Menus/nav_bar_evento.php' ?>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="area_org.php">Hyper Events</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="../../area_org.php">Home<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="lista_eventos.php">Meus eventos</a>
+                        </li>  
+                    </ul>
+                </div>
+                <form class="form-inline my-2 my-lg-0">
+                    <a href="../Controls/logout.php" class="btn btn-danger">Sair</a>      
+                </form>
+            </nav>
             <div class="text-right mx-auto" style="margin: 20px">
                 <a href="../Cadastros/cadastro_de_evento.php" class="btn btn-primary btn-lg text-right"> Cadastrar Evento </a>
             </div>
