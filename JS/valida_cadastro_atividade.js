54b29c148ab8c3923ed0212665540e5b7909035e
Kauan <kauanphbbb@gmail.com>
2019-09-30 00:57:01 -0300

    Validando a hora inicial e final

--- JS/valida_cadastro_atividade.js
+++ JS/valida_cadastro_atividade.js
@@ -1,8 +1,13 @@
 function validaCadastro() {
     var name_class_data = document.forms['form_cadastro_atividade'].data.className;
+    var name_class_hora_fim = document.forms['form_cadastro_atividade'].hora_fim.className;
 
     if (name_class_data.match(/is-invalid/)) {
         document.forms['form_cadastro_atividade'].data.className = name_class_data.replace('is-invalid', ' ');
+    }
+
+    if (name_class_hora_fim.match(/is-invalid/)) {
+        document.forms['form_cadastro_atividade'].hora_fim.className = name_class_data.replace('is-invalid', ' ');
     }
 
     function valida_data() {
@@ -58,10 +63,56 @@ function validaCadastro() {
         return true;
     }
 
+    function validaHoras() {
+        var name_class = document.forms['form_cadastro_atividade'].hora_fim.className;
+        var new_class_name = name_class + ' is-invalid';
+
+        //Pegando o horário inicial
+        var horario_inicial = document.forms['form_cadastro_atividade'].hora_inicio.value;
+
+        //Pegando o horário final
+        var horario_final = document.forms['form_cadastro_atividade'].hora_fim.value;
+
+        //Separando o horário inicial em horas e minutos
+        var horas_inicio = horario_inicial.substring(0, 2);
+        var minutos_inicio = horario_inicial.substring(3, 6);
+
+        //Separando o horário final em horas e minutos
+        var horas_fim = horario_final.substring(0, 2);
+        var minutos_fim = horario_final.substring(3, 6);
+
+        //Convertendo o horário inicial para números
+        var horas_inicio = Number(horas_inicio);
+        var minutos_inicio = Number(minutos_inicio);
+
+        //Convertendo o horário final para números
+        var horas_fim = Number(horas_fim);
+        var minutos_fim = Number(minutos_fim);
+
+        //Verificando se a hora é válida
+        if (horas_fim < horas_inicio) {
+            document.forms['form_cadastro_atividade'].hora_fim.className = new_class_name;
+            return false;
+        }
+
+        //Verificando se os minutos são válidos
+        else if (horas_fim == horas_inicio && minutos_fim < minutos_inicio) {
+            document.forms['form_cadastro_atividade'].hora_fim.className = new_class_name;
+            return false;
+        }
+        else if (horas_fim == horas_inicio && minutos_fim == minutos_inicio) {
+            document.forms['form_cadastro_atividade'].hora_fim.className = new_class_name;
+            return false;
+        }
+        
+        return true;
+    }
+
     var data = valida_data();
+    var hora = validaHoras();
 
     var erro = [];
-    var lista = [data];
+    var lista = [data, hora];
     for (i in lista) {
         erro.push(lista[i]);
     }
