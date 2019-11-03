15a018b7b2efadc9e10fbc82b0347281761248c8
KastroWalker <victorcsa2002@gmail.com>
2019-11-02 22:20:23 -0300

    Adicionando a opção de apagar convidado

--- Controls/CRUD/gerencia_convidado.php
+++ Controls/CRUD/gerencia_convidado.php
@@ -4,14 +4,14 @@
     
 	$acao = $_REQUEST['acao'];
 
-    $evento_id = $_SESSION['id_evento'];
-    $nome = mysqli_real_escape_string($conexao, trim($_POST['campo_nome']));
-    $email = mysqli_real_escape_string($conexao, trim($_POST['campo_email']));
-    $contato = mysqli_real_escape_string($conexao, trim($_POST['campo_telefone']));
-	$contato = str_replace('-', '', $contato);
-	$contato = str_replace(' ', '', $contato);
-    $tipo_convidado = mysqli_real_escape_string($conexao, trim($_POST['tipo_convidado']));
-    $descricao = mysqli_real_escape_string($conexao, trim($_POST['campo_descricao']));
+    @$evento_id = $_SESSION['id_evento'];
+    @$nome = mysqli_real_escape_string($conexao, trim($_POST['campo_nome']));
+    @$email = mysqli_real_escape_string($conexao, trim($_POST['campo_email']));
+    @$contato = mysqli_real_escape_string($conexao, trim($_POST['campo_telefone']));
+	@$contato = str_replace('-', '', $contato);
+	@$contato = str_replace(' ', '', $contato);
+    @$tipo_convidado = mysqli_real_escape_string($conexao, trim($_POST['tipo_convidado']));
+    @$descricao = mysqli_real_escape_string($conexao, trim($_POST['campo_descricao']));
    	
 	if($acao == "cadastrar"){
 	    $sql = "insert into convidado(idTipoConvidado ,evento_id ,nome_convidado ,descricao ,email ,contato) values ('{$tipo_convidado}', '{$evento_id}', '{$nome}', '{$descricao}', '{$email}', '{$contato}');";
@@ -27,5 +27,25 @@
 		header('Location: ../../views/Eventos/Cadastros/cadastro_convidado.php');
 		exit();
 		$conexao->close();
-	}else if($acao == "editar")
+	}else if($acao == "deletar"){
+		$convidado_id = $_POST['delete_id'];
+
+		if (isset($_POST['deletedata'])) {
+			$sql = "delete from convidado where idConvidado = $convidado_id";
+
+			$result = mysqli_query($conexao, $sql);
+
+			if(!$result){
+				$_SESSION['erro_excluir'] = true;
+				die('Erro: '.mysqli_error($conexao));
+			}else{
+				$_SESSION['sucesso_excluir'] = true;
+			}
+			header('Location: ../../views/Eventos/Listar/lista_convidados.php');
+			
+			$conexao->close();
+			
+			exit();
+		}
+	}
 ?>
--- Controls/Listar/lista_convidados.php
+++ Controls/Listar/lista_convidados.php
@@ -8,6 +8,8 @@
 		<th>Id: </th>
 		<th>Nome: </th>
 		<th>Tipo: </th>
+		<th>EDITAR</th>
+		<th>APAGAR</th>
 	</tr>
 <?php
 	$result = mysqli_query($conexao, $sql);
@@ -21,8 +23,11 @@
 		mysqli_error($conexao);
 		echo "<tr>";
 		echo "<td>$indice</td>";
-		echo "<td><a href='../informacoes_convidado.php?convidado_id=$id'>$nome</a></td>";
+		echo "<td style='display: none;'>$id</td>";
 		echo "<td>$tipo_convi</td>";
+		echo "<td><a href='../informacoes_convidado.php?convidado_id=$id'>$nome</a></td>";
+		echo "<td><button type='button' class='btn btn-success editbtn'>Editar</button></td>";
+		echo "<td><button type='button' class='btn btn-danger deletebtn'>Apagar</button></td>";
 		echo "</tr>";
 
 		$indice++;
