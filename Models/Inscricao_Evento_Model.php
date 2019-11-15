00827fabcc2ac47bf17a9b1b03b3d56cd9037d58
kauanphbbb <kauanphbbb@gmail.com>
2019-11-15 18:27:17 -0300

    Model de inscrição em eventos

---
+++ Models/Inscricao_Evento_Model.php
@@ -0,0 +1,44 @@
+<?php
+class Inscricao_Evento_Model{
+    private $evento_id;
+    private $user_id;
+    private $data_inscricao_evento;
+    private $hora_inscricao_evento;
+
+    function __construct(){
+        $this->evento_id = 0;
+        $this->user_id = 0;
+        $this->data_inscricao_evento = "aaaa/mm/dd";
+        $this->hora_inscricao_evento = "00:00";
+    }
+
+    function getEventoId(){
+        return $this->evento_id;
+    }
+    function setEventoId($evento_id){
+        $this->evento_id = $evento_id;
+    }
+
+    function getUserId(){
+        return $this->user_id;
+    }
+    function setUserId($user_id){
+        $this->user_id = $user_id;
+    }
+
+    function getDataInscricaoEvento(){
+        return $this->data_inscricao_evento;
+    }
+    function setDataInscricaoEvento($data_inscricao_evento){
+        $this->data_inscricao_evento = $data_inscricao_evento;
+    }
+
+    function getHoraInscricaoEvento(){
+        return $this->hora_inscricao_evento;
+    }
+    function setHoraInscricaoEvento($hora_inscricao_evento){
+        $this->hora_inscricao_evento = $hora_inscricao_evento;
+    }
+}
+
+?>
