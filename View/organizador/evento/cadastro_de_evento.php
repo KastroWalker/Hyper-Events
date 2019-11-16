17921a4d1622409f0307d645b02bdd65a7d44e55
KastroWalker <victorcsa2002@gmail.com>
2019-11-15 23:40:40 -0300

    Criando o cadastro de evento

--- View/organizador/evento/cadastro_de_evento.php
+++ View/organizador/evento/cadastro_de_evento.php
@@ -1,7 +1,6 @@
 <?php
 # Inicia a sessão
 session_start();
-# Importa a função para ver se o usuário esta logado
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -14,21 +13,17 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta name="author" content="Victor Castro"/> 
 
-    <link rel="stylesheet" type="text/css" href="../../../CSS/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../../../CSS/style_cadastro_eveto.css">
-    <link rel="stylesheet" type="text/css" href="../../../CSS/style_padrao.css">
+    <link rel="stylesheet" type="text/css" href="../../../lib/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../lib/css/style_cadastro_eveto.css">
+    <link rel="stylesheet" type="text/css" href="../../../lib/css/style_padrao.css">
     <link rel="icon" href="../../../img/icon.png" type="image/x-icon" />
-    
-    <script src="../../../JS/jquery.js"></script>
-    <script src="../../../JS/formata.js"></script>
-    <script src="../../../JS/valida_cadastro_evento.js"></script>
     
     <title>Cadastrar Evento - Hyper-Events</title>
 </head>
 
 <body>
     <?php
-    require_once '../../header_eventos.php';
+        include 'header.php';;
     ?>
     <main>
         <section id="Cadastrar_Evento">
@@ -39,7 +34,7 @@ session_start();
                 ?>
                 <div class="alert alert-success text-center">
                     Evento Cadastrado com Sucesso!<br />
-                    Clique <a href="../Listar/lista_eventos.php"><strong>aqui</strong></a> para ver os eventos;
+                    Clique <a href="../lista_eventos.php"><strong>aqui</strong></a> para ver os eventos;
                 </div>
                 <?php
             }
@@ -69,7 +64,7 @@ session_start();
             unset($_SESSION['erro_cadastrado']);
             ?>
 
-            <form method="POST" action="../../../Controls/CRUD/gerencia_evento.php?acao=cadastrar" id="form_cadastro" onsubmit="return valida_cadastro();">
+            <form method="POST" action="../../../Control/Evento_Control.php?acao=cadastrar" id="form_cadastro" onsubmit="return valida_cadastro();">
                 <h2>Cadastrar Evento</h2>
                 <div class="form-row">
                     <div class="col-md-6">
@@ -125,15 +120,17 @@ session_start();
             <br />
             <button type="submit" class="btn btn-primary">Cadastrar</button>
             <button type="reset" class="btn btn-secondary">Limpar</button>
-            <a class="btn btn-info" href="../Listar/lista_eventos.php">Voltar</a>
+            <a class="btn btn-info" href="../lista_eventos.php">Voltar</a>
             <p><strong>Atenção: </strong>Todos os campos que possuem '*' são obrigatorios.</p>
         </form>
     </section>
 </main>
-<?php
-    # Importa o rodape padrão
-require_once '../../footer.php';
-?>
+    <script src="../../../lib/js/jquery.js"></script>
+    <script src="../../../lib/js/formata.js"></script>
+    <script src="../../../lib/js/valida_cadastro_evento.js"></script>
+    <?php
+        require_once '../../footer.php';
+    ?>
 </body>
 
 </html>
