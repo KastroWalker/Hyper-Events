370180836b6ce8843efe424faea0a7f60b4e61d5
kauanphbbb <kauanphbbb@gmail.com>
2019-12-02 00:36:45 -0300

    selecionando de inscrição evento

--- SQL/Selects.sql
+++ SQL/Selects.sql
@@ -9,3 +9,8 @@ inner join eventos on (eventos.evento_id = atividade.evento_id) and eventos.even
 select convidado.nome_convidado, convidado.idConvidado, tipoConvidado.tipo_convidado, eventos.evento_id
 from convidado inner join tipoConvidado on (convidado.idTipoConvidado = tipoConvidado.idTipoConvidado)
 inner join eventos on (eventos.evento_id = convidado.evento_id) and eventos.evento_id = 1;
+
+-- Listando o nome do evento e o nome do usuário de inscrição evento --
+select usuario.nome, eventos.titulo_evento, inscricao_evento.matricula
+from inscricao_evento inner join eventos on (inscricao_evento.evento_id = eventos.evento_id)
+inner join usuario on (inscricao_evento.user_id = usuario.user_id) and inscricao_evento.matricula = 1;
